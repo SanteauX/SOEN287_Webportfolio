3d0a43e2307f7edd7a33b8486ddb4517995a46c1
SanteauX <hugo.joncour.k2o@gmail.com>
2020-02-24 00:22:49 -0500

    update

--- Python/app.py
+++ Python/app.py
@@ -5,10 +5,30 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def default():
+    return render_template("./html/index.html")
 
 @app.route('/index')
+def index():
+    return render_template("./html/index.html")
+
+@app.route('/whoami')
+def whoami():
+    return render_template('./HTML/template.html')
+
+@app.route('/skills')
+def skills():
+    return render_template("./HTML/skills.html")
+
+@app.route('/blog')
+def blog():
+    return render_template("./HTML/blog.html")
+
+@app.route('/cv')
+def cv():
+    return render_template('./HTML/cv.html')
+
+@app.route('/contact')
 def contact():
-    return render_template('./HTML/Index.html')
+    return render_template('./HTML/contact.html')
+
