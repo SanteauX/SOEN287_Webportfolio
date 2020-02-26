3e571e1d9645fb8cb057ca3c6a523c87a1773dfc
SanteauX <welson.thumper@gmail.com>
2020-02-26 00:25:37 -0500

    update

--- Python/app.py
+++ Python/app.py
@@ -1,34 +1,42 @@
-
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../html")
 
 
 @app.route('/')
 def default():
-    return render_template("./html/index.html")
+    return render_template("index.html")
+
 
 @app.route('/index')
 def index():
-    return render_template("./html/index.html")
+    return render_template("index.html")
+
 
 @app.route('/whoami')
 def whoami():
-    return render_template('./HTML/template.html')
+    loremIpsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the " \
+                 "industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type " \
+                 "and scrambled it to make a type specimen book. It has survived not only five centuries, " \
+                 "but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised " \
+                 "in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently " \
+                 "with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "
 
-@app.route('/skills')
-def skills():
-    return render_template("./HTML/skills.html")
+    list = [loremIpsum, loremIpsum, loremIpsum, loremIpsum]
 
-@app.route('/blog')
-def blog():
-    return render_template("./HTML/blog.html")
+    return render_template("template.html", list = list)
 
-@app.route('/cv')
-def cv():
-    return render_template('./HTML/cv.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('./HTML/contact.html')
 
+    return render_template("template.html")
+
+
+@app.route('/skills')
+def skills():
+    return render_template("template.html")
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000, debug=True)
