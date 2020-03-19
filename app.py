5f380028aeffc158a3aa46cdf60821478f18e066
SanteauX <hugo.joncour.k2o@gmail.com>
2020-03-19 01:53:13 -0400

    created login and signup

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
+import os
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
-
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/hugo/Desktop/webportfolio/data/blog.db'
-db = SQLAlchemy(app)
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    subtitle = db.Column(db.String(50))
-    author = db.Column(db.String(50))
-    date_posted = db.Column(db.DateTime)
-    content = db.Column(db.Text)
-
-
-
+bootstrap = Bootstrap(app)
 
 moi1 = "static/img/Me/moi.jpg"
 moi2 = "static/img/Me/moi2.jpg"
 moi3 = "static/img/Me/moi3.jpg"
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 projects = open("data/github_projects.csv")
 project_lines = projects.readlines()
@@ -30,64 +21,95 @@ for i in range(0, len(project_lines)):
     project_lines[i] = project_lines[i].split(",")
 
 
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=3, max=50)])
+    password = StringField('password', validators=[InputRequired(), Length(min=12, max=80)])
+
+
+class LoginForm(FlaskForm):
+    username = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=12, max=80)])
+    remember = BooleanField("Remember me")
+
+
 @app.route('/')
 def default():
     return render_template("/home.html")
 
-@app.route('/index.html')
+
+@app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route('/post.html')
+
+@app.route('/post')
 def post():
     return render_template("post.html")
 
-@app.route('/login.html')
-def login():
-    return render_template("login.html")
 
-@app.route('/home.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return"<h1>"+form.username.data + " " + form.password.data + "</h1>"
+    return render_template("login.html", form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return"<h1>"+form.username.data + " " + form.email.data + " " + form.password.data + "</h1>"
+    return render_template("signup.html", form=form)
+
+
+@app.route('/home')
 def home():
     return render_template("home.html")
 
-@app.route('/whoami.html')
+
+@app.route('/whoami')
 def whoami():
     return render_template("whoami.html",
-    image=moi1,
-    image2=moi2,
-    image3=moi3)
+                           image=moi1,
+                           image2=moi2,
+                           image3=moi3)
 
-@app.route('/skills.html')
+
+@app.route('/skills')
 def skills():
     return render_template("skills.html")
 
-@app.route('/blog.html')
+
+@app.route('/blog')
 def blog():
     return render_template("blog.html")
 
-@app.route('/projects.html')
+
+@app.route('/projects')
 def projects():
     return render_template("projects.html",
-    lines=project_lines[1:],
-    github_url="https://github.com/",
-    number=len(project_lines)-1)
+                           lines=project_lines[1:],
+                           github_url="https://github.com/",
+                           number=len(project_lines) - 1)
 
-@app.route('/cv.html')
+
+@app.route('/cv')
 def cv():
     return render_template("cv.html")
 
-@app.route('/contact.html')
+
+@app.route('/contact')
 def contact():
     return render_template("contact.html")
 
 
+# app name
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html", home="/home.html")
 
-
-
-# app name 
-@app.errorhandler(404) 
-def not_found(e): 
-    return render_template("404.html", home="/home.html") 
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8000, debug=True)
