2159d6a33ff5b403f1b3b5f01201178305589c36
SanteauX <hugo.joncour.k2o@gmail.com>
2020-03-27 02:00:15 -0400

    minor changes

--- mongo.py
+++ mongo.py
@@ -3,10 +3,12 @@ from pymongo import MongoClient
 import ssl
 
 
-uri = "mongodb+srv://HugoAdmin:<yvctrd6F7GUYBVYT>@personalsite-3gjka.mongodb.net/test?retryWrites=true&w=majority??ssl=true&ssl_cert_reqs=CERT_NONE"
-cluster = MongoClient("uri")
-db = cluster['Site']
-collection = db['BlogPosts']
+client = MongoClient("mongodb+srv://HugoAdmin:<yvctrd6F7GUYBVYT>@personalsite-3gjka.mongodb.net/test?retryWrites=true&w=majority", ssl_cert_reqs=ssl.CERT_NONE)
+db = client.test()
+db = client['Site']
 
-post = {"_id": 0, "author": "Hugo Joncour", "date": "25/03/2020", "Title": "test", "Subtitle": "subtitle test", "tags": ["CS", "ECON"], "body": ["part 1", "part 2"], "images": ["image 1", "image 2"]}
+collection = db['BlogPosts']
+#print(collection)
+post = {"_id": 0, "author": "Hugo Joncour", "date": "25/03/2020"}
+#, "Title": "test", "Subtitle": "subtitle test", "tags": ["CS", "ECON"], "body": ["part 1", "part 2"], "images": ["image 1", "image 2"]}
 collection.insert_one(post)
